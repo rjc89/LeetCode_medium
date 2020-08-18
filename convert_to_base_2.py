712e99bc96b33584d61d1137e0104879a4f41ae9
Robert <rjconstable1989@gmail.com>
2020-08-18 22:39:23 +0100

    added reference

--- convert_to_base_2.py
+++ convert_to_base_2.py
@@ -1,5 +1,7 @@
 from typing import List
 
+#https://leetcode.com/problems/convert-to-base-2/discuss/265507/JavaC%2B%2BPython-2-lines-Exactly-Same-as-Base-2
+
 # baseNeg2 is the same as base2
 class Solution:
     def base2(self, x):
