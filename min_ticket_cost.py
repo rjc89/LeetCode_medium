f55458dd32fe4260803654fc32908ccfa5a6fb74
Robert <rjconstable1989@gmail.com>
2020-08-20 21:51:23 +0100

    min tickets costs

---
+++ min_ticket_cost.py
@@ -0,0 +1,8 @@
+#https://leetcode.com/problems/minimum-cost-for-tickets/
+from typing import List
+
+class Solution:
+    def mincostTickets(self, days: List[int], costs: List[int]) -> int:
+
+s = Solution()
+print(s.mincostTickets())
