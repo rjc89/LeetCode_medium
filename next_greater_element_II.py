4a25b2094fdaecea463b56c029a94a9777280314
Robert <rjconstable1989@gmail.com>
2020-10-08 21:29:17 +0100

    yup

---
+++ next_greater_element_II.py
@@ -0,0 +1,6 @@
+# https://leetcode.com/problems/next-greater-element-ii/
+class Solution:
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        
+s = Solution()
+print(s.nextGreaterElements([1,2,1]))
