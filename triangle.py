4a3e249aae28a9a11c39327011a571649b7ea4bd
Robert <rjconstable1989@gmail.com>
2020-11-04 20:39:45 +0000

    triangle

---
+++ triangle.py
@@ -0,0 +1,20 @@
+#https://leetcode.com/problems/triangle/
+from typing import List
+
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        height = len(triangle)
+        distance = triangle[0]*height
+        for h in range(1, height):
+            for i in range(h,-1,-1):
+                choose_path = min(distance[max(i-1,0):min(i+1,h)])
+                distance[i] = triangle[h][i] + choose_path
+
+        print(min(distance))
+        return min(distance)
+
+s = Solution()
+s.minimumTotal(
+[[-1],[2,3],[1,-1,-3]]
+)
+        
