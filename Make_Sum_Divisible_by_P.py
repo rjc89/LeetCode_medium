828e7f78f11955ee4fa16e394f7da455b591c5b2
Robert <rjconstable1989@gmail.com>
2020-11-19 21:49:42 +0000

    subarray sum

---
+++ Make_Sum_Divisible_by_P.py
@@ -0,0 +1,15 @@
+# class Solution:
+#     def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+#         ans = []
+#         max_candy = max(candies)
+#         for i, j in enumerate(candies):
+#             if j+extraCandies >= max_candy:
+#                 ans.append([True])
+#             else:
+#                 ans.append(False)
+#         return ans
+
+class Solution:
+    def minSubarray(self, nums: List[int], p: int) -> int:
+        return 
+    
