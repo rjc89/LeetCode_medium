48de8c4a73e6000983d7712815dc73fe7aa43220
Robert <rjconstable1989@gmail.com>
2020-11-20 22:33:06 +0000

    working on it

--- Make_Sum_Divisible_by_P.py
+++ Make_Sum_Divisible_by_P.py
@@ -9,7 +9,37 @@
 #                 ans.append(False)
 #         return ans
 
+# class Solution:
+#     def shuffle(self, nums: List[int], n: int) -> List[int]:
+#         first = nums[:n]
+#         second = nums[n:]
+#         ans = []
+#         for i in range(0,n,1):
+#             ans.append(first[i])
+#             ans.append(second[i])
+        
+#         return ans
+# https://leetcode.com/problems/make-sum-divisible-by-p/
+
+from typing import List
+
 class Solution:
     def minSubarray(self, nums: List[int], p: int) -> int:
-        return 
+        total = sum(nums)
+        remainder = total % p
+        if not remainder:
+            return 0
+        ans = float('inf')
+        presum = [0]
+        remainders = {0: -1}
+        for i, n in enumerate(nums):
+            presum.append(presum[-1] + n)
+            r = ((cr := presum[-1] % p) - remainder) % p
+            if r in remainders:
+                ans = min(ans, i - remainders[r])
+            remainders[cr] = i
+        return ans if ans < len(nums) else -1
+
+s = Solution()
+s.minSubarray(nums = [6,3,5,2], p = 9)
     
