dc649b2a610759331502e3174a1dd5a939faa3a9
Robert Constable <rjconstable1989@gmail.com>
2021-02-04 20:17:46 +0000

    ones and zeroes

---
+++ ones_and_zeroes.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/ones-and-zeroes/discuss/701736/Python-DP-Solution-explained-with-example
+from typing import List
+
+class Solution:
+    def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
+        dp = [[0] * (n+1) for _ in range(m+1)]
+        for s in strs:
+            zeros, ones = s.count("0"), s.count("1")
+            for i in range(m, zeros -1, -1):
+                for j in range(n, ones - 1, - 1):
+                    dp[i][j] = max (1 + dp[i - zeros][j - ones], dp[i][j])
+        return dp[-1][-1]
+
+s = Solution()
+print(s.findMaxForm(strs = ["10","0001","111001","1","0"], m = 5, n = 3))       
